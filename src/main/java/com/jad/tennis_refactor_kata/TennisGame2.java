a1e25152a69e55c2997789a1789ff8bc1e3bf7cf
Jean-Aymeric <jeanaymeric@gmail.com>
2024-05-31 11:54:28 +0200

    rename all methods, variables and attributes

--- src/main/java/com/jad/tennis_refactor_kata/TennisGame2.java
+++ src/main/java/com/jad/tennis_refactor_kata/TennisGame2.java
@@ -2,141 +2,124 @@ package com.jad.tennis_refactor_kata;
 
 
 public class TennisGame2 implements TennisGame {
-    public int P1point = 0;
-    public int P2point = 0;
-
-    public String P1res = "";
-    public String P2res = "";
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
+    public int player1Points = 0;
+    public int player2Points = 0;
+    public String player1Score = "";
+    public String player2Score = "";
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
-    public void SetP1Score(int number) {
-
-        for (int i = 0; i < number; i++) {
-            P1Score();
-        }
-
-    }
-
-    public void P1Score() {
-        P1point++;
-    }
-
-    public void SetP2Score(int number) {
-
-        for (int i = 0; i < number; i++) {
-            P2Score();
-        }
-
-    }
-
-    public void P2Score() {
-        P2point++;
-    }
-
-    public void wonPoint(String player) {
-        if (player == "player1") {
-            P1Score();
+    public void wonPoint(String playerName) {
+        if (playerName == "player1") {
+            this.incrementPlayer1Points();
         } else {
-            P2Score();
+            this.incrementPlayer2Points();
         }
+    }
+
+    public void incrementPlayer1Points() {
+        this.player1Points++;
+    }
+
+    public void incrementPlayer2Points() {
+        this.player2Points++;
     }
 
     public String getScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4) {
-            if (P1point == 0) {
+        if (this.player1Points == this.player2Points && this.player1Points < 4) {
+            if (this.player1Points == 0) {
                 score = "Love";
             }
-            if (P1point == 1) {
+            if (this.player1Points == 1) {
                 score = "Fifteen";
             }
-            if (P1point == 2) {
+            if (this.player1Points == 2) {
                 score = "Thirty";
             }
             score += "-All";
         }
-        if (P1point == P2point && P1point >= 3) {
+        if (this.player1Points == this.player2Points && this.player1Points >= 3) {
             score = "Deuce";
         }
 
-        if (P1point > 0 && P2point == 0) {
-            if (P1point == 1) {
-                P1res = "Fifteen";
+        if (this.player1Points > 0 && this.player2Points == 0) {
+            if (this.player1Points == 1) {
+                this.player1Score = "Fifteen";
             }
-            if (P1point == 2) {
-                P1res = "Thirty";
+            if (this.player1Points == 2) {
+                this.player1Score = "Thirty";
             }
-            if (P1point == 3) {
-                P1res = "Forty";
-            }
-
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1) {
-                P2res = "Fifteen";
-            }
-            if (P2point == 2) {
-                P2res = "Thirty";
-            }
-            if (P2point == 3) {
-                P2res = "Forty";
+            if (this.player1Points == 3) {
+                this.player1Score = "Forty";
             }
 
-            P1res = "Love";
-            score = P1res + "-" + P2res;
+            this.player2Score = "Love";
+            score = this.player1Score + "-" + this.player2Score;
+        }
+        if (this.player2Points > 0 && this.player1Points == 0) {
+            if (this.player2Points == 1) {
+                this.player2Score = "Fifteen";
+            }
+            if (this.player2Points == 2) {
+                this.player2Score = "Thirty";
+            }
+            if (this.player2Points == 3) {
+                this.player2Score = "Forty";
+            }
+
+            this.player1Score = "Love";
+            score = this.player1Score + "-" + this.player2Score;
         }
 
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2) {
-                P1res = "Thirty";
+        if (this.player1Points > this.player2Points && this.player1Points < 4) {
+            if (this.player1Points == 2) {
+                this.player1Score = "Thirty";
             }
-            if (P1point == 3) {
-                P1res = "Forty";
+            if (this.player1Points == 3) {
+                this.player1Score = "Forty";
             }
-            if (P2point == 1) {
-                P2res = "Fifteen";
+            if (this.player2Points == 1) {
+                this.player2Score = "Fifteen";
             }
-            if (P2point == 2) {
-                P2res = "Thirty";
+            if (this.player2Points == 2) {
+                this.player2Score = "Thirty";
             }
-            score = P1res + "-" + P2res;
+            score = this.player1Score + "-" + this.player2Score;
         }
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2) {
-                P2res = "Thirty";
+        if (this.player2Points > this.player1Points && this.player2Points < 4) {
+            if (this.player2Points == 2) {
+                this.player2Score = "Thirty";
             }
-            if (P2point == 3) {
-                P2res = "Forty";
+            if (this.player2Points == 3) {
+                this.player2Score = "Forty";
             }
-            if (P1point == 1) {
-                P1res = "Fifteen";
+            if (this.player1Points == 1) {
+                this.player1Score = "Fifteen";
             }
-            if (P1point == 2) {
-                P1res = "Thirty";
+            if (this.player1Points == 2) {
+                this.player1Score = "Thirty";
             }
-            score = P1res + "-" + P2res;
+            score = this.player1Score + "-" + this.player2Score;
         }
 
-        if (P1point > P2point && P2point >= 3) {
+        if (this.player1Points > this.player2Points && this.player2Points >= 3) {
             score = "Advantage player1";
         }
 
-        if (P2point > P1point && P1point >= 3) {
+        if (this.player2Points > this.player1Points && this.player1Points >= 3) {
             score = "Advantage player2";
         }
 
-        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
+        if (this.player1Points >= 4 && this.player2Points >= 0 && (this.player1Points - this.player2Points) >= 2) {
             score = "Win for player1";
         }
-        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
+        if (this.player2Points >= 4 && this.player1Points >= 0 && (this.player2Points - this.player1Points) >= 2) {
             score = "Win for player2";
         }
         return score;
